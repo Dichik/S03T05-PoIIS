15dfbfc282f1726dc345ddc7937aa1989d5ab8ad
Dichik <omeluan.dima@gmail.com>
2022-12-17 15:54:55 +0200

    assignment #4: complete base implementation with errors

--- monte-carlo-tree-search/tree/node.py
+++ monte-carlo-tree-search/tree/node.py
@@ -23,29 +23,41 @@ class MonteCarloTreeSearchNode:
             self._untried_actions = list(self.state.legal_moves)
         return self._untried_actions
 
+    @property
+    def q(self):
+        wins = self._results[self.parent.state]
+        loses = self._results[-1 * self.parent.state]
+        return wins - loses
+
     def best_child(self, c_param=1.4):
-        # here we calculating UCT (Upper Confidence Bound applied to trees)
-        # and after that choose the best child among all available
-        scores = [0 for c in self.children]
-        return self.children[0]
+        choices_weights = [
+            (c.q / c._number_of_visits) + c_param * np.sqrt((2 * np.log(self._number_of_visits) / c._number_of_visits))
+            for c in self.children
+        ]
+        return self.children[np.argmax(choices_weights)]
 
     def rollout_policy(self, possible_moves):
         return possible_moves[np.randint(len(possible_moves))]
 
     def expand(self):
-        pass
-        # take from untried_actions the first one
         action = self._untried_actions.pop()
+        next_state = self.state.push(action)
+        child_node = MonteCarloTreeSearchNode(state=next_state, parent=self)
+        self.children.append(child_node)
+        return child_node
 
-    def is_terminal_node(self):
-        return self.state.is_checkmate() == True \
-               or self.state.is_variant_draw() == True
+    def is_terminal_node(self, state=None):
+        if state is None:
+            return self.state.is_checkmate() or self.state.is_variant_draw()
+        else:
+            return state.is_checkmate() or state.is_variant_draw()
 
     def rollout(self):
-        current_state = self.state
-        while not current_state.is_terminal_state():
-            action = self.rollout_policy(current_state.legal_moves)
-            current_rollout_state = self.state.push(action)
+        current_rollout_state = self.state
+        while not self.is_terminal_node(current_rollout_state):
+            possible_moves = current_rollout_state.legal_moves
+            action = self.rollout_policy(possible_moves)
+            current_rollout_state = current_rollout_state.push(action)
         return current_rollout_state
 
     def backpropagate(self, result):
